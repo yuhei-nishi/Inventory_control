fab96370b1d127e1468087782e062ff20f639257
Yuhei Nishi <abc-gtgtg.21q.abds@ezweb.ne.jp>
2020-06-04 17:53:09 +0900

    在庫登録機能実装
    （項目）
    商品名
    購入日
    在庫数
    賞味期限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if  @item.save
+      redirect_to root_path, notice: '出品完了しました'
+    else
+      flash[:alert] = '出品できませんでした'
+      redirect_to action: "new"
+    end
   end
 end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :purchase, :stock, :expiration)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "groups#index"
   resources :groups, only: [:show, :new]
-  resources :items, only: [:index, :new, :show]
+  resources :items, only: [:index, :new, :create]
 end
