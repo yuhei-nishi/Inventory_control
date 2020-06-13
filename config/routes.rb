5573d1a2241d39844e992a7465829a40328ee7e9
Yuhei Nishi <abc-gtgtg.21q.abds@ezweb.ne.jp>
2020-06-13 23:36:30 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:index, :edit, :update]
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
 end
